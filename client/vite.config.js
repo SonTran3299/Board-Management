2384eb0eed71550994189e6b55509b34609634f0
SonTran3299 <tranvanson0302@gmail.com>
2026-06-20 14:49:20 +0700

    fix: update vite config base path for vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   envDir: '../',
-//   base: command === 'build' ? '/Board-Management/' : '/',
-// })
-
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
     envDir: '../',
-    base: command === 'build' ? '/Board-Management/' : '/',
+    //base: command === 'build' ? '/Board-Management/' : '/',
+    base: '/',
   }
 })
